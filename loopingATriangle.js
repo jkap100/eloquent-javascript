989600489dadb4e975ddb464e699e3134030a013
Jon Kaplon <jon.kaplon1@gmail.com>
2022-07-26 08:39:13 -0700

    looping a triangle

---
+++ loopingATriangle.js
@@ -0,0 +1,10 @@
+const loopingATriangle = (a) => {
+  let triangle = "";
+  for (let i = 0; i <= 7; i++) {
+    triangle += "#";
+    console.log(triangle);
+  }
+};
+
+a = 7;
+console.log(loopingATriangle(a));
